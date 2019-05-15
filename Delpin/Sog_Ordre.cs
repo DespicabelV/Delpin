60074e9e3d6861dc15d6ed73ed48c1e7e42945d9
Viggo Lysdahl <Viggo_lysdahl@hotmail.com>
2019-05-15 12:21:14 +0200

    Første Commit, alle klasser og Forms oprret samt temp filer til hvert gruppe medlem

--- Delpin/Sog_Ordre.cs
+++ Delpin/Sog_Ordre.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Delpin
 {
-    public partial class Form1 : Form
+    public partial class Sog_Ordre : Form
     {
-        public Form1()
+        public Sog_Ordre()
         {
             InitializeComponent();
         }
