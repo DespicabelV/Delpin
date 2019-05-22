f524436d047656845ab6a9fe00b8c919433ea050
Katrine Andersen <Katrine.77@hotmail.com>
2019-05-22 11:59:19 +0200

    Lejelisten virker og små ændringer

--- Delpin/Controller.cs
+++ Delpin/Controller.cs
@@ -3,38 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Delpin
 {
     class Controller
     {
+        Database_Manager dbm = new Database_Manager();
 
-       
-        public static void GemDEB(long cprCvr, string navn, string gade, int postnr, string by, int tlf, string email, string ansvarlig)
+        public void GemDEB(long cprCvr, string navn, string gade, int postnr, string by, int tlf, string email, string ansvarlig)
         {
-            Database_Manager dbm = new Database_Manager();
+            
             DEB deb = new DEB(cprCvr, navn, gade, postnr, by, tlf, email, ansvarlig);
             dbm.IndsætDEB(deb);
         }
 
-        public static DEB HentDEB(long cprCvr)
+        public DEB HentDEB(long cprCvr)
         {
-            Database_Manager dbm = new Database_Manager();
             DEB deb = dbm.HentDEB(cprCvr);
             return deb;
         }
 
-        public static void SletDEB(long cprCvr)
+        public void SletDEB(long cprCvr)
         {
-            Database_Manager dbm = new Database_Manager();
             dbm.SletDEB(cprCvr);
         }
 
-        public static void UpdateDEB(long cprCvr, string navn, string gade, int postnr, string by, int tlf, string email, string ansvarlig)
+        public void UpdateDEB(long cprCvr, string navn, string gade, int postnr, string by, int tlf, string email, string ansvarlig)
         {
-            Database_Manager dbm = new Database_Manager();
             DEB deb = new DEB(cprCvr, navn, gade, postnr, by, tlf, email, ansvarlig);
             dbm.UpdateDEB(deb);
+        }
+
+        public string SkrivOpretFil(string filnavn, string start, string slut)
+        {
+            //Opretter filnavn. Filnavnet hedder startdato + slutdato. 
+            filnavn = $"{filnavn}.txt";
+            StreamWriter outputStream = null;
+            try
+            {
+                outputStream = new StreamWriter(filnavn);
+                List<string> linje = new List<string>();
+
+                //Der laves en liste som bliver udfyldt med informationer fra databasen. 
+                linje = dbm.HentLejeListe(start, slut);
+                for (int i = 0; i < linje.Count; i++)
+                {
+                    //Udskriver hver linje i listen til en fil.
+                    outputStream.WriteLine(linje[i]);
+
+                }
+
+                return "Liste hentet, gå til filer for at åbne den.";
+            }
+            catch (FileNotFoundException)
+            {
+                return "Fejl: Text-filen findes ikke.";
+            }
+            finally
+            {
+                if (outputStream != null)
+                    outputStream.Close();
+            }
+
         }
 
     }
