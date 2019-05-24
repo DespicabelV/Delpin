59439c314c9e448c4594da152a0e0e4668b3bd43
Viggo Lysdahl <Viggo_lysdahl@hotmail.com>
2019-05-24 09:35:41 +0200

    Revert "Test af revert filer"
    This reverts commit 830d4bd285eb28bcfcede1d6d8ca99dafa69f466.

--- Delpin/Sog_RES.Designer.cs
+++ Delpin/Sog_RES.Designer.cs
@@ -29,148 +29,135 @@
         private void InitializeComponent()
         {
             this.buttonTilføjRes = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Navn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Pris = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Start_Dato = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Slut_Dato = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.buttonSog = new System.Windows.Forms.Button();
             this.dateTimePickerSlut = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
             this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBoxResNavn = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxKategori = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBoxHovedKategori = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonTilføjRes
             // 
-            this.buttonTilføjRes.Location = new System.Drawing.Point(20, 492);
-            this.buttonTilføjRes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonTilføjRes.Location = new System.Drawing.Point(15, 400);
             this.buttonTilføjRes.Name = "buttonTilføjRes";
-            this.buttonTilføjRes.Size = new System.Drawing.Size(109, 47);
+            this.buttonTilføjRes.Size = new System.Drawing.Size(82, 38);
             this.buttonTilføjRes.TabIndex = 17;
             this.buttonTilføjRes.Text = "Tilføj Res";
             this.buttonTilføjRes.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Navn,
-            this.Pris,
-            this.Start_Dato,
-            this.Slut_Dato});
-            this.dataGridView1.Location = new System.Drawing.Point(20, 73);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(641, 411);
-            this.dataGridView1.TabIndex = 16;
-            // 
-            // Navn
-            // 
-            this.Navn.HeaderText = "Navn";
-            this.Navn.Name = "Navn";
-            this.Navn.ReadOnly = true;
-            // 
-            // Pris
-            // 
-            this.Pris.HeaderText = "Pris";
-            this.Pris.Name = "Pris";
-            this.Pris.ReadOnly = true;
-            // 
-            // Start_Dato
-            // 
-            this.Start_Dato.HeaderText = "Start Dato";
-            this.Start_Dato.Name = "Start_Dato";
-            this.Start_Dato.ReadOnly = true;
-            // 
-            // Slut_Dato
-            // 
-            this.Slut_Dato.HeaderText = "Slut Dato";
-            this.Slut_Dato.Name = "Slut_Dato";
-            this.Slut_Dato.ReadOnly = true;
-            // 
             // buttonSog
             // 
-            this.buttonSog.Location = new System.Drawing.Point(95, 37);
-            this.buttonSog.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSog.Location = new System.Drawing.Point(99, 35);
             this.buttonSog.Name = "buttonSog";
-            this.buttonSog.Size = new System.Drawing.Size(104, 28);
+            this.buttonSog.Size = new System.Drawing.Size(78, 23);
             this.buttonSog.TabIndex = 15;
             this.buttonSog.Text = "Søg";
             this.buttonSog.UseVisualStyleBackColor = true;
+            this.buttonSog.Click += new System.EventHandler(this.buttonSog_Click);
             // 
             // dateTimePickerSlut
             // 
-            this.dateTimePickerSlut.Location = new System.Drawing.Point(395, 39);
-            this.dateTimePickerSlut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePickerSlut.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePickerSlut.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerSlut.Location = new System.Drawing.Point(329, 29);
             this.dateTimePickerSlut.Name = "dateTimePickerSlut";
-            this.dateTimePickerSlut.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePickerSlut.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerSlut.TabIndex = 14;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(340, 43);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(288, 35);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(32, 17);
+            this.label3.Size = new System.Drawing.Size(25, 13);
             this.label3.TabIndex = 13;
             this.label3.Text = "Slut";
             // 
             // dateTimePickerStart
             // 
-            this.dateTimePickerStart.Location = new System.Drawing.Point(395, 7);
-            this.dateTimePickerStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePickerStart.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(329, 6);
             this.dateTimePickerStart.Name = "dateTimePickerStart";
-            this.dateTimePickerStart.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePickerStart.Size = new System.Drawing.Size(200, 20);
             this.dateTimePickerStart.TabIndex = 12;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(340, 11);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(288, 12);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 17);
+            this.label2.Size = new System.Drawing.Size(29, 13);
             this.label2.TabIndex = 11;
             this.label2.Text = "Start";
-            // 
-            // textBoxResNavn
-            // 
-            this.textBoxResNavn.Location = new System.Drawing.Point(95, 7);
-            this.textBoxResNavn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBoxResNavn.Name = "textBoxResNavn";
-            this.textBoxResNavn.Size = new System.Drawing.Size(236, 22);
-            this.textBoxResNavn.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.Size = new System.Drawing.Size(81, 13);
             this.label1.TabIndex = 9;
-            this.label1.Text = "Res Navn";
+            this.label1.Text = "Hoved Kategori";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Kategori";
+            // 
+            // comboBoxKategori
+            // 
+            this.comboBoxKategori.FormattingEnabled = true;
+            this.comboBoxKategori.Location = new System.Drawing.Point(99, 65);
+            this.comboBoxKategori.Name = "comboBoxKategori";
+            this.comboBoxKategori.Size = new System.Drawing.Size(178, 21);
+            this.comboBoxKategori.TabIndex = 19;
+            this.comboBoxKategori.SelectedIndexChanged += new System.EventHandler(this.comboBoxKategori_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(525, 228);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // comboBoxHovedKategori
+            // 
+            this.comboBoxHovedKategori.FormattingEnabled = true;
+            this.comboBoxHovedKategori.Location = new System.Drawing.Point(99, 9);
+            this.comboBoxHovedKategori.Name = "comboBoxHovedKategori";
+            this.comboBoxHovedKategori.Size = new System.Drawing.Size(178, 21);
+            this.comboBoxHovedKategori.TabIndex = 21;
+            this.comboBoxHovedKategori.SelectedIndexChanged += new System.EventHandler(this.comboBoxHovedKategori_SelectedIndexChanged);
             // 
             // Sog_RES
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 554);
-            this.Controls.Add(this.buttonTilføjRes);
+            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.Controls.Add(this.comboBoxHovedKategori);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBoxKategori);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonTilføjRes);
             this.Controls.Add(this.buttonSog);
             this.Controls.Add(this.dateTimePickerSlut);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.dateTimePickerStart);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBoxResNavn);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Sog_RES";
             this.Text = "Sog_RES";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -182,17 +169,15 @@
         #endregion
 
         private System.Windows.Forms.Button buttonTilføjRes;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Navn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Pris;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Start_Dato;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Slut_Dato;
         private System.Windows.Forms.Button buttonSog;
         private System.Windows.Forms.DateTimePicker dateTimePickerSlut;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.DateTimePicker dateTimePickerStart;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxResNavn;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxKategori;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxHovedKategori;
     }
 }
